f5ab3edfd019f226f2b9dd2592cba5ef97dfa18e
ejungwoo <phyjics@gmail.com>
2020-07-21 21:31:22 +0900

    draw_from_summary.C

--- mary/draw_from_summary.C
+++ mary/draw_from_summary.C
@@ -5,8 +5,14 @@ void draw_from_summary()
 {
   ejungwoo::gstat(0);
 
-  bool ana_mult = true;
-  bool ana_val = true;
+  bool ana_mult = 1;
+  bool ana_val = 1;
+
+  vector<TString> versionlist = {
+    //"NewAna.2034.45b9400",
+    //"NewAna.2057.254c9ba",
+    "NewAna.2070.0231288",
+  };
 
   TString fParticleNames[6] = {"p","d","t","he3","he4","he6"};
   double fParticleMass[6] = {938.272, 1871.06, 2809.41, 2809.41, 3728.4, 5606.55};
@@ -18,13 +24,15 @@ void draw_from_summary()
   ejungwoo::binning bmult3 (40,0,40);
   ejungwoo::binning bkeoa  (40,0,400);
   ejungwoo::binning bptoa  (200,0,1200);
-  ejungwoo::binning bny    (200,-1.5,3.0);
+  ejungwoo::binning bny    (200,-2.,2.0);
   ejungwoo::binning btta   (200,0,180);
   ejungwoo::binning bphi   (200,-180,180);
   ejungwoo::binning bpozlab(200,0,2500);
   ejungwoo::binning bdedx  (200,0,1500);
 
-  ejungwoo::variable var_eff     ("eff","eff");
+  ejungwoo::variable("prob","prob");
+  ejungwoo::variable("eff","eff");
+
   ejungwoo::variable var_keoa_cm ("keoa_cm" ,"ke_cm/$$(a)"                ,"$$(gcut)" ,"$$(pname)_$$(sys);KE_{CM}/A (MeV);N_{tracks} / N_{events}"    ,bkeoa);
   ejungwoo::variable var_ptoa_cm ("ptoa_cm" ,"pt_cm/$$(a)"                ,"$$(gcut)" ,"$$(pname)_$$(sys);p_{T}/A (MeV/c);N_{tracks} / N_{events}"    ,bptoa);
   ejungwoo::variable var_ny_cm   ("ny_cm"   ,"ny_cm"                      ,"$$(gcut)" ,"$$(pname)_$$(sys);y_{CM}/y_{beam,CM};N_{tracks} / N_{events}" ,bny);
@@ -33,31 +41,32 @@ void draw_from_summary()
   ejungwoo::variable var_poz_lab ("poz_lab" ,"p_lab/$$(z)"                ,""         ,"$$(pname)_$$(sys);p_{Lab}/Z (MeV/c^{2})"                      ,bpozlab);
   ejungwoo::variable var_dedx    ("dedx"    ,"dedx"                       ,""         ,"$$(pname)_$$(sys);dE/dx"                                      ,bdedx);
 
-  ejungwoo::variable var_n    ("n"    ,"n"    ,"" ,"all_$$(sys);N_{prob > 0.5}",bmult );
+  ejungwoo::variable var_n    ("n"    ,"np+nd+ nt+nhe3+nhe4"    ,"" ,"all_$$(sys);N_{prob > 0.5}",bmult );
   ejungwoo::variable var_np   ("np"   ,"np"   ,"" ,  "p_$$(sys);N_{prob > 0.5}",bmult3);
   ejungwoo::variable var_nd   ("nd"   ,"nd"   ,"" ,  "d_$$(sys);N_{prob > 0.5}",bmult3);
   ejungwoo::variable var_nt   ("nt"   ,"nt"   ,"" ,  "t_$$(sys);N_{prob > 0.5}",bmult3);
   ejungwoo::variable var_nhe3 ("nhe3" ,"nhe3" ,"" ,"he3_$$(sys);N_{prob > 0.5}",bmult3);
   ejungwoo::variable var_nhe4 ("nhe4" ,"nhe4" ,"" ,"he4_$$(sys);N_{prob > 0.5}",bmult3);
-  ejungwoo::variable var_ng    ("n_good"    ,"n_good"    ,"" ,"all_$$(sys);N_{prob > 0.8, eff > 0.1, abs(sd) < 3}",bmult2);
-  ejungwoo::variable var_ngp   ("np_good"   ,"np_good"   ,"" ,  "p_$$(sys);N_{prob > 0.8, eff > 0.1, abs(sd) < 3}",bmult2);
-  ejungwoo::variable var_ngd   ("nd_good"   ,"nd_good"   ,"" ,  "d_$$(sys);N_{prob > 0.8, eff > 0.1, abs(sd) < 3}",bmult2);
-  ejungwoo::variable var_ngt   ("nt_good"   ,"nt_good"   ,"" ,  "t_$$(sys);N_{prob > 0.8, eff > 0.1, abs(sd) < 3}",bmult2);
-  ejungwoo::variable var_nghe3 ("nhe3_good" ,"nhe3_good" ,"" ,"he3_$$(sys);N_{prob > 0.8, eff > 0.1, abs(sd) < 3}",bmult2);
-  ejungwoo::variable var_nghe4 ("nhe4_good" ,"nhe4_good" ,"" ,"he4_$$(sys);N_{prob > 0.8, eff > 0.1, abs(sd) < 3}",bmult2);
 
+  ejungwoo::variable var_ng    ("n_good"    ,"n_good"    ,"" ,"all_$$(sys);N_{prob > $$(prob_cut), eff > $$(eff_cut), abs(sd) < $$(sd_cut)}",bmult3);
+  ejungwoo::variable var_ngp   ("np_good"   ,"np_good"   ,"" ,  "p_$$(sys);N_{prob > $$(prob_cut), eff > $$(eff_cut), abs(sd) < $$(sd_cut)}",bmult2);
+  ejungwoo::variable var_ngd   ("nd_good"   ,"nd_good"   ,"" ,  "d_$$(sys);N_{prob > $$(prob_cut), eff > $$(eff_cut), abs(sd) < $$(sd_cut)}",bmult2);
+  ejungwoo::variable var_ngt   ("nt_good"   ,"nt_good"   ,"" ,  "t_$$(sys);N_{prob > $$(prob_cut), eff > $$(eff_cut), abs(sd) < $$(sd_cut)}",bmult2);
+  ejungwoo::variable var_nghe3 ("nhe3_good" ,"nhe3_good" ,"" ,"he3_$$(sys);N_{prob > $$(prob_cut), eff > $$(eff_cut), abs(sd) < $$(sd_cut)}",bmult2);
+  ejungwoo::variable var_nghe4 ("nhe4_good" ,"nhe4_good" ,"" ,"he4_$$(sys);N_{prob > $$(prob_cut), eff > $$(eff_cut), abs(sd) < $$(sd_cut)}",bmult2);
 
-  vector<ejungwoo::variable> varlist = { var_ny_cm+var_ptoa_cm, var_poz_lab+var_dedx, };
+  vector<ejungwoo::variable> varlist_is = { var_ny_cm };
+  vector<ejungwoo::variable> varlist1 = { var_phi_cm+var_tta_cm, var_ny_cm+var_ptoa_cm, var_poz_lab+var_dedx, };
   vector<ejungwoo::variable> varlist2 = { var_n+var_ng, var_np+var_ngp, var_nd+var_ngd, var_nt+var_ngt, var_nhe3+var_nghe3, var_nhe4+var_nghe4, };
   vector<ejungwoo::variable> varlist3 = { var_np, var_nd, var_nt, var_nhe3, var_nhe4, };
   vector<ejungwoo::variable> varlist4 = { var_ngp, var_ngd, var_ngt, var_nghe3, var_nghe4, };
 
-  ejungwoo::gsetcut(TCut("($$(tta_cm)<110)/$$(eff)/$$(num_events)"));
+  //ejungwoo::gsetcut(TCut("($$(tta_cm)<110)/$$(eff)/$$(num_events)"));
+  //ejungwoo::gsetcut(TCut("1./$$(eff)/$$(num_events)"));
+  ejungwoo::gsetcut(TCut("1.*$$(prob)/$$(eff)/$$(num_events)*($$(tta_cm)<110)"));
   //ejungwoo::gsetcut(TCut("($$(tta_cm)<110)"));
 
-  //for (auto version : {"NewAna.2034.45b9400" ,"NewAna.2057.254c9ba"})
-  //for (auto version : {"NewAna.2057.254c9ba"})
-  for (auto version : {"NewAna.2034.45b9400"})
+  for (auto version : versionlist)
   {
     TString vshort = TString("v")+ejungwoo::tok(version,".",1);
     ejungwoo::gversion(version);
@@ -70,9 +79,13 @@ void draw_from_summary()
       int isys = (sys==132?0:1);
       ejungwoo::setpar("sys",sys);
 
-      //TString fileName = Form("data_summary/mult_all_%d.%s.root",sys,ejungwoo::versioncc());
-      TString fileName = Form("data_summary2/summary_all_%d.%s.root",sys,ejungwoo::versioncc());
+      //TString fileName = Form("data__%s/summary_all_%d.%s.root",ejungwoo::versioncc(),sys,ejungwoo::versioncc());
+      TString fileName = Form("data__%s/sys%d.%s.ana.particle.root",ejungwoo::versioncc(),sys,ejungwoo::versioncc());
       auto file = new TFile(fileName);
+      ejungwoo::setpar("prob_cut", ((TParameter<double> *) file -> Get("prob_cut")) -> GetVal());
+      ejungwoo::setpar("eff_cut", ((TParameter<double> *) file -> Get("eff_cut")) -> GetVal());
+      ejungwoo::setpar("sd_cut", ((TParameter<double> *) file -> Get("sd_cut")) -> GetVal());
+
       auto tree = (TTree *) file -> Get("mult");
       num_events[isys] = tree -> GetEntries();
       ejungwoo::setpar("num_events",num_events[isys]);
@@ -81,7 +94,9 @@ void draw_from_summary()
       if (ana_mult) {
         ejungwoo::gheader(vshort+"_"+sys+"_");
 
-        TCanvas *cvs = ejungwoo::div(ejungwoo::cc((vshort+"_"+sys),1500,1000),3,2);
+        TCanvas *cvs = nullptr;
+        if (varlist2.size()>0)
+          cvs = ejungwoo::div(ejungwoo::cc(TString("mult")+vshort+"_"+sys,1500,1000),3,2);
 
         int ivar = 0;
         for (auto var : varlist2) {
@@ -97,12 +112,12 @@ void draw_from_summary()
 
       if (ana_val)
       {
-        TString fileName = Form("data_summary2/summary_all_%d.%s.root",sys,ejungwoo::versioncc());
-        auto file = new TFile(fileName);
+        //TString fileName = Form("data_summary2/summary_all_%d.%s.root",sys,ejungwoo::versioncc());
+        //auto file = new TFile(fileName);
 
         int ivar = 0;
         TCanvas *cvs[10] = {0};
-        for (auto var : varlist) {
+        for (auto var : varlist1) {
           cvs[ivar] = ejungwoo::div(ejungwoo::cc((var.name+"_"+vshort+"_"+sys),1500,1000),3,2);
           ivar++;
         }
@@ -115,13 +130,13 @@ void draw_from_summary()
           ejungwoo::setpar("z",fParticleZ[ipid]);
           auto tree = (TTree *) file -> Get(fParticleNames[ipid]);
 
-          for (auto var : {var_keoa_cm})  {
-          //histKE[ipid][isys] = (TH1D *) (ejungwoo::drawv(var,tree)).hist;
+          for (auto var : varlist_is) {
+            var.binn.n=100;
             histKE[ipid][isys] = (TH1D *) ejungwoo::tp(var,tree);
           }
 
           int ivar = 0;
-          for (auto var : varlist) {
+          for (auto var : varlist1) {
             auto hist = ejungwoo::tp(var,tree);
             
             TString cname = var.name+"_"+vshort+"_"+sys+"_"+ipid;
@@ -138,7 +153,12 @@ void draw_from_summary()
       TString cname_ke = TString("KE_")+vshort;
       auto cvs = ejungwoo::div(ejungwoo::cc(cname_ke,1500,1000),3,2);
       TString cname_is = TString("IS_")+vshort;
-      //ejungwoo::addto(cname_is,graphis, "pl", fParticleNames[ipid]);
+
+      auto ttl = varlist_is[0].title;
+      ttl.y = "p(132+124)/p(108+112)";
+      auto bbb = varlist_is[0].binn;
+
+      ejungwoo::addto(cname_is,ejungwoo::new_h("",ttl,bbb,ejungwoo::binning(200,0,2)),"addx");
       for (int ipid : {0,1,2,3,4})
       {
         ejungwoo::setpar("pname",fParticleNames[ipid]);
@@ -154,6 +174,7 @@ void draw_from_summary()
   }
 
   ejungwoo::drawall();
+  //ejungwoo::saveall("pdf");
 }
 
 //TGraphErrors *draw_is(int idx_particle, TH1 *hist132, int num_events_132, TH1 *hist108, int num_events_108, int num_tracks_cut)
